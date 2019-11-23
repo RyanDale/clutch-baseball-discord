d4b7fc8abad3be993ddbb9bc08dddd7ae741dd62
Ryan Dale <rdale@jbssolutions.com>
2019-11-23 17:12:45 -0500

    Update roll to support adding a value.

--- commands/general/roll.js
+++ commands/general/roll.js
@@ -6,14 +6,18 @@ class RollCommand extends Commando.Command {
 			name: 'roll',
 			group: 'general',
 			memberName: 'roll',
-			description: 'Roll die. Example: /r 20 rolls a random number between 1 and 20',
+			description: 'Roll die. Example: /r d20 + 4 rolls a random number between 1 and 20 and adds 4',
 			aliases: ['r']
 		});
 	}
 
 	async run(message, searchText) {
-		const dieRoll = Math.floor(Math.random() * parseInt(searchText, 10)) + 1;
-		return message.reply(`rolled a ${dieRoll}`);
+		const [roll, add] = searchText.split('+');
+		const parsedRoll = parseInt(roll.replace(/[^0-9]/gi, ''), 10); 
+		const parsedAdd = parseInt(add || 0, 10);
+		const dieRoll = Math.floor(Math.random() * parsedRoll) + 1;
+		const output = add ? `rolled a ${dieRoll} + ${parsedAdd} = ${dieRoll + parsedAdd}` : `rolled a ${dieRoll}`;
+		return message.reply(output);
 	}
 }
 
