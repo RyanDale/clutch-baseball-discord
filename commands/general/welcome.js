8c9042fede29884e5bdbd00064122ba1faf45540
Ryan Dale <rdale@jbssolutions.com>
2019-11-28 18:28:05 -0500

    Remove /rule command line.

--- commands/general/welcome.js
+++ commands/general/welcome.js
@@ -3,8 +3,7 @@ const Commando = require('discord.js-commando');
 const welcomeMessage = '\n• There are a variety of channels you can join using the Channels (+) button on the left. \
 They are broken up into 3 categories... Official Channels, Community Channels, and Field Channels.\n \
 • There is a Clutch Baseball bot that has commands to trigger images from in channels.  DM the bot with `/help` for \
-more information.\n • You can use the `/rules` command in Direct Messages with the bot or in Channels to see a variety \
-of options for pulling info from the rulebook.\n • You can use the `/roll` command for rolls. For example, `/roll d20` \
+more information.\n • You can use the `/roll` command for rolls. For example, `/roll d20` \
 for a d20, or adding in modifiers such as `/roll d20+5` will work as well.\n • For extra fun, use `/giphy` (whatever you \
 want to see) to add some flair (or some salt) to your matchup.\n • You can play games via Direct Message, but for \
 easier tracking (and to use the #rules feature), and so others can spectate, hop into any unused Field channel (like \
