c591aad165d930641fb2ed86041bb105d4b794b0
Ryan Dale <rdale@jbssolutions.com>
2019-11-29 13:54:33 -0500

    Add support for complex die rolls.

--- commands/cards/roll.js
+++ commands/cards/roll.js
@@ -12,11 +12,12 @@ class RollCommand extends Commando.Command {
 	}
 
 	async run(message, searchText) {
-		const [roll, add] = searchText.split('+');
-		const parsedRoll = parseInt(roll.replace(/[^0-9]/gi, ''), 10); 
-		const parsedAdd = parseInt(add || 0, 10);
-		const dieRoll = Math.floor(Math.random() * parsedRoll) + 1;
-		const output = add ? `rolled a ${dieRoll} + ${parsedAdd} = ${dieRoll + parsedAdd}` : `rolled a ${dieRoll}`;
+		const [roll, extra] = searchText.split(/[-+](.+)/)
+		const operator = searchText.split('').find(character => character === '+' || character === '-') || '';
+		const dieRoll = Math.floor(Math.random() * parseInt(roll.replace(/[^0-9]/gi, ''), 10)) + 1;
+		const extraResult = eval(operator + extra) || 0;
+		const result = dieRoll + extraResult;
+		const output = operator ? `rolled a ${dieRoll} ${operator}${extra} = ${result}` : `rolled a ${dieRoll}`;
 		return message.reply(output);
 	}
 }
